7dab927a688b70b53737eb5da341d596067831c1
Vodhrin <78246218+Vodhrin@users.noreply.github.com>
2021-05-12 11:13:31 -0500

    Finishing touches before new release.

---
+++ StanWorshipper_VSProject/Core/Hooks.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using RoR2;
+
+namespace StanWorshipper.Core
+{
+    internal static class Hooks
+    {
+        public static void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            orig(self);
+
+            if (self)
+            {
+                if (self.HasBuff(Survivor.Buffs.sacrificeBuff))
+                {
+                    self.moveSpeed += self.moveSpeed * Survivor.States.Sacrifice.moveSpeedBoost;
+                }
+            }
+        }
+
+        public static void HealthComponent_TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
+        {
+
+            if (damageInfo == null) { orig(self, damageInfo); return; }
+
+            CharacterBody characterBody = self.gameObject.GetComponent<CharacterBody>();
+            CharacterBody attackerCharacterBody = damageInfo.attacker.GetComponent<CharacterBody>();
+
+            if (attackerCharacterBody.baseNameToken == "STRONGSTANFRAGMENT_NAME" || attackerCharacterBody.baseNameToken == "WEAKSTANFRAGMENT_NAME")
+            {
+                damageInfo.attacker = attackerCharacterBody.master.minionOwnership.ownerMaster.GetBody().gameObject;
+            }
+            orig(self, damageInfo);
+        }
+
+        public static void BuffCatalog_Init(On.RoR2.BuffCatalog.orig_Init orig)
+        {
+            orig();
+            Survivor.Buffs.GetBuffIndices();
+        }
+    }
+}
--- StanWorshipper_VSProject/StanWorshipperPlugin.cs
+++ StanWorshipper_VSProject/StanWorshipperPlugin.cs
@@ -80,44 +80,9 @@ namespace StanWorshipper
 
         private void CreateHooks()
         {
-            On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
-            On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
-            On.RoR2.BuffCatalog.Init += BuffCatalog_Init;
-        }
-
-        private void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
-        {
-            orig(self);
-
-            if (self)
-            {
-                if (self.HasBuff(Survivor.Buffs.sacrificeBuff))
-                {
-                    self.moveSpeed += self.moveSpeed * Survivor.States.Sacrifice.moveSpeedBoost;
-                }
-            }
-        }
-
-        private void HealthComponent_TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
-        {
-
-            if (damageInfo == null) { orig(self, damageInfo); return; }
-
-            CharacterBody characterBody = self.gameObject.GetComponent<CharacterBody>();
-            CharacterBody attackerCharacterBody = damageInfo.attacker.GetComponent<CharacterBody>();
-
-            if (attackerCharacterBody.baseNameToken == "STRONGSTANFRAGMENT_NAME")
-            {
-                damageInfo.attacker = attackerCharacterBody.master.minionOwnership.ownerMaster.GetBody().gameObject;
-                logger.LogMessage(attackerCharacterBody.master.minionOwnership.ownerMaster.GetBody().gameObject);
-            }
-            orig(self, damageInfo);
-        }
-
-        private void BuffCatalog_Init(On.RoR2.BuffCatalog.orig_Init orig)
-        {
-            orig();
-            Survivor.Buffs.GetBuffIndices();
+            On.RoR2.CharacterBody.RecalculateStats += Core.Hooks.CharacterBody_RecalculateStats;
+            On.RoR2.HealthComponent.TakeDamage += Core.Hooks.HealthComponent_TakeDamage;
+            On.RoR2.BuffCatalog.Init += Core.Hooks.BuffCatalog_Init;
         }
     }
 }
